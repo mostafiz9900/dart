10e15d7eea74e9de2e93490a8ddab434c4098f85
Md. Mostafizur Rahman <mostafiz9900@gmail.com>
2021-12-04 10:23:54 +0600

    completed

--- arrow_function/main.dart
+++ arrow_function/main.dart
@@ -1,3 +1,10 @@
 void main() {
-  print('Hello Dart');
+  String getName = fullName('Mostafizur', 'Rahman');
+  print(getName);
+  print(fullName('Mir Minhan'));
+  print(sumNum(10,30));
 }
+
+String fullName(String fName, [String lName = '']) => fName + " " + lName;
+
+var sumNum=(int h, int w) => 20 + (h*w);
